f6eb887051ee9d39dfb2d2613e70ca14c26d5586
PedroKcz <pedro.kcz@gmail.com>
2022-04-04 21:27:10 -0300

    Set up project with refresh versions

---
+++ settings.gradle.kts
@@ -0,0 +1,30 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        google()
+        mavenCentral()
+    }
+}
+
+plugins {
+    id("de.fayard.refreshVersions") version "0.40.1"
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+refreshVersions {
+    enableBuildSrcLibs()
+    rejectVersionIf {
+        candidate.stabilityLevel.isLessStableThan(current.stabilityLevel)
+    }
+}
+
+rootProject.name = "Hyrule Compendium"
+
+include(":app")
