09878d02a375973ebf56a976ce3e141d8785141d
PedroKcz <pedro.kcz@gmail.com>
2022-04-07 09:44:30 -0300

    Create viewmodel and state

---
+++ features/categories/src/main/java/com/hyrule/features/categories/presentation/CompendiumCategoriesState.kt
@@ -0,0 +1,7 @@
+package com.hyrule.features.categories.presentation
+
+import com.hyrule.features.categories.domain.entity.CompendiumCategory
+
+data class CompendiumCategoriesState(
+    val categories: List<CompendiumCategory>
+)
