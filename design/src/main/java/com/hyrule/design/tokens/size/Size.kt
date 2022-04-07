4b04537f0f390307a6cff850b3ba378a37cdd334
PedroKcz <pedro.kcz@gmail.com>
2022-04-07 12:22:26 -0300

    Create size constants

---
+++ design/src/main/java/com/hyrule/design/tokens/size/Size.kt
@@ -0,0 +1,10 @@
+package com.hyrule.design.tokens.size
+
+import androidx.compose.ui.unit.dp
+
+object Size {
+    val hairline = 1.dp
+    val small = 24.dp
+    val medium = 48.dp
+    val giant = 96.dp
+}
